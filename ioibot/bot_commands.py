080cfe189b1776f55651753a38a1f4fd92045257
Gregorius Emmanuel Henry <emmanuelhenry2002@gmail.com>
2022-06-03 12:51:30 +0700

    Remove HTC from accounts command permission

--- ioibot/bot_commands.py
+++ ioibot/bot_commands.py
@@ -120,7 +120,7 @@ class Command:
             await self.invite()
 
         elif self.command.startswith("accounts"):
-            if user.role not in ['Leader', 'Deputy Leader', 'HTC']:
+            if user.role not in ['Leader', 'Deputy Leader']:
                 await send_text_to_room(
                     self.client, self.room.room_id,
                     "Only Leader and Deputy Leader can use this command."
